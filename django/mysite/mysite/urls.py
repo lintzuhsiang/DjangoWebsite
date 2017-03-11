17940c12efa45ef0dadee45a638724a2dc4e457a
lintzyhsiang <b02901069@ntu.edu.tw>
2017-03-11 22:19:33 +0800

    seconde vision

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from trips.views import hello_world
+from trips.views import hello_world,home
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'hello/$',hello_world),
+    url(r'^$',home),
 ]
--- django/mysite/trips/views.py
+++ django/mysite/trips/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-
+from .models import Exercise
 # Create your views here.
 def hello_world(request):
 	return render(request,'hello_world.html',{'current_time':str(datetime.now()),
 })
+
+def home(request):
+	exercise_list = Exercise.objects.all()
+	return render(request,'home.html',{'exercise_list':exercise_list})
